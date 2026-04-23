26f0dc258f420c6fcb12e2b382614d3c3bb0c5cf
Valerii Voinarovych <87275444+vvoinarovych@users.noreply.github.com>
2026-04-23 19:59:32 +0200

    Add: kth-largest-integer-in-a-stream - submission-0

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-0.java
@@ -0,0 +1,23 @@
+class KthLargest {
+    int k;
+    PriorityQueue<Integer> pq;
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        this.pq = new PriorityQueue<>();
+        for(int num : nums){
+            pq.offer(num);
+            if(pq.size() > k){
+                pq.poll();
+            }
+
+        }
+    }
+    
+    public int add(int val) {
+        pq.offer(val);
+         if(pq.size() > k){
+                pq.poll();
+        }
+        return pq.peek();
+    }
+}
