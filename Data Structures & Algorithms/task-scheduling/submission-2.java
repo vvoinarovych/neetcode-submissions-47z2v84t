b280df57fc86fa76422f3ad30be09e5471258fb4
Valerii Voinarovych <87275444+vvoinarovych@users.noreply.github.com>
2026-05-05 07:10:50 +0200

    Add: task-scheduling - submission-2

---
+++ Data Structures & Algorithms/task-scheduling/submission-2.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        int[] count = new int[26];
+        for(char task : tasks){
+            count[task - 'A']++;
+        }
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for(int cnt : count){
+            if(cnt > 0){
+               pq.offer(cnt);
+            }
+        }
+        ArrayDeque<int[]> q = new ArrayDeque<>();
+        int time = 0;
+        while(!pq.isEmpty() || !q.isEmpty()){
+            time++;
+            if(!pq.isEmpty()){
+                int val = pq.poll() - 1;
+                if(val > 0){
+                    q.offer(new int[]{val, time + n});
+                }
+            }
+            if(!q.isEmpty() && q.peek()[1] == time){
+                pq.offer(q.poll()[0]);
+            }
+        }
+        return time;
+    }
+}
