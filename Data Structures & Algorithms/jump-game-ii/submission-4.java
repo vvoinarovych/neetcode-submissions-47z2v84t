5dba44da1d88983ead48c56ffda0e7f0a5a21278
Valerii Voinarovych <87275444+vvoinarovych@users.noreply.github.com>
2026-07-17 12:04:43 +0200

    Add: jump-game-ii - submission-4

---
+++ Data Structures & Algorithms/jump-game-ii/submission-4.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int jump(int[] nums) {
+        int l = 0;
+        int r = 0;
+        int jumps = 0;
+        while(r < nums.length - 1){
+            int farthest = 0;
+            for(int i = l; i <= r; i++){
+                farthest = Math.max(farthest, nums[i] + i);
+            }
+            
+            l = r + 1;
+            r = farthest;
+            jumps++;
+        }
+        return jumps;
+    }
+}
