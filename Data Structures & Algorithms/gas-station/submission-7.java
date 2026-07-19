6af6f3c0d4e255e4d16a7b16a786a301dbc13901
Valerii Voinarovych <87275444+vvoinarovych@users.noreply.github.com>
2026-07-19 19:55:43 +0200

    Add: gas-station - submission-7

---
+++ Data Structures & Algorithms/gas-station/submission-7.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        if(Arrays.stream(gas).sum() < Arrays.stream(cost).sum()) return -1;
+        int balance = 0;
+        int id = 0;
+        for(int i = 0; i < gas.length; i++){
+            balance += gas[i] - cost[i];
+            if(balance < 0){
+                id = i + 1;
+                balance = 0;
+            }
+            
+        }
+        return id;
+    }
+}
