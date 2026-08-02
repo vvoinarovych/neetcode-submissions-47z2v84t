83b1751ae466d8660bcf9129cbe648e1ef9d7202
Valerii Voinarovych <87275444+vvoinarovych@users.noreply.github.com>
2026-08-02 20:35:13 +0200

    Add: meeting-schedule - submission-0

---
+++ Data Structures & Algorithms/meeting-schedule/submission-0.java
@@ -0,0 +1,24 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+    public boolean canAttendMeetings(List<Interval> intervals) {
+        intervals.sort((a, b) -> a.start - b.start);
+        for(int i = 1; i < intervals.size(); i++){
+            var cur = intervals.get(i);
+            var prev = intervals.get(i - 1);
+            if(cur.start < prev.end){
+                return false;
+            }
+        }
+        return true;
+    }
+}
