2805b027c4b22f5b76d93cf12d136485b6b10a77
Valerii Voinarovych <87275444+vvoinarovych@users.noreply.github.com>
2026-08-04 20:35:28 +0200

    Add: non-overlapping-intervals - submission-6

---
+++ Data Structures & Algorithms/non-overlapping-intervals/submission-6.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int eraseOverlapIntervals(int[][] intervals) {
+        Arrays.sort(intervals, (a,b) -> Integer.compare(a[0], b[0]));
+        int r = intervals[0][1];
+        int counter = 0;
+        for(int i = 1; i < intervals.length; i++){
+            int[] iv = intervals[i];
+
+            if(r > iv[0]){
+                r = Math.min(r, iv[1]);
+                counter++;
+            }else{
+                r = iv[1];
+            }
+        }
+        return counter;
+    }
+}
