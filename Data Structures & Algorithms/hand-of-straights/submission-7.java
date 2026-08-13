b0b0101b3dde267cc7e1daf271534c1077bc42e0
Valerii Voinarovych <87275444+vvoinarovych@users.noreply.github.com>
2026-08-13 22:59:04 +0200

    Add: hand-of-straights - submission-7

---
+++ Data Structures & Algorithms/hand-of-straights/submission-7.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isNStraightHand(int[] hand, int groupSize) {
+        if(hand.length % groupSize != 0) return false;
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        for(int h : hand){
+            map.put(h, map.getOrDefault(h, 0) + 1);
+        }
+
+        for(int i = 0; i < hand.length / groupSize; i++){
+            int first = map.firstKey();
+            for(int j = 0; j < groupSize; j++){
+                if(!map.containsKey(first + j)) return false;
+                if(map.get(first + j) > 1){
+                    map.put(first + j, map.get(first + j)  - 1);
+                }else{
+                    map.remove(first + j);
+                }
+            }
+        }
+        return true;
+    }
+}
