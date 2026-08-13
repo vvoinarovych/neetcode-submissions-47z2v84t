e04a8f2b990d59edc129e14e2941eb010b80ed2c
Valerii Voinarovych <87275444+vvoinarovych@users.noreply.github.com>
2026-08-13 19:41:24 +0200

    Add: buy-and-sell-crypto - submission-7

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-7.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int max = 0;
+        int cur = 0;
+        int min = Integer.MAX_VALUE;
+        for(int p : prices){
+            min = Math.min(min, p);
+            max = Math.max(max, p - min);            
+        }
+        return max;
+    }
+}
