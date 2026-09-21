55bf824fc41c0bce6c4f37b5ad3ba74c12f863b5
Valerii Voinarovych <87275444+vvoinarovych@users.noreply.github.com>
2026-09-21 16:03:37 +0200

    Add: climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/climbing-stairs/submission-2.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int climbStairs(int n) {
+        int[] cache = new int[n + 1];
+        cache[0] = 1;
+        cache[1] = 1;
+        for(int i = 2; i <= n; i++){
+            cache[i] = cache[i-1] + cache[i-2]; 
+        }
+        return cache[n];
+    }
+}
