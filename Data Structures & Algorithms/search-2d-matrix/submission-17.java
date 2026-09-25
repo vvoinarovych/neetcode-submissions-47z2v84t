8931cc4386f91300e11c4072ef8035c313343430
Valerii Voinarovych <87275444+vvoinarovych@users.noreply.github.com>
2026-09-25 13:46:28 +0200

    Add: search-2d-matrix - submission-17

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-17.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int ROWS = matrix.length;
+        int COLS = matrix[0].length;
+
+        int l = 0;
+        int r = ROWS * COLS - 1;
+
+        while(l <= r){
+            int mid = (l + r) / 2;
+            int row = mid / COLS;
+            int col = mid % COLS;
+            if(matrix[row][col] == target){
+                return true;
+            }else if(matrix[row][col] < target){
+                l = mid + 1;
+            }else{
+                r = mid - 1;
+            }
+        }
+        return false;
+    }
+}
